c94a08c00206d33e15f477dbc70d54104cf09e74
KaushalLMAO <gautamkaushal381@gmail.com>
2025-01-29 08:35:30 +0545

    //
    ///

---
+++ copify/copify.js
@@ -0,0 +1,15 @@
+// data
+const song_data = [
+  {
+    id: 1,
+    song_name: "Blue",
+  },
+];
+//DOM
+let noti = document.querySelector(".noti");
+
+// noti function
+
+noti.addEventListener("click", (notiFunction) => {
+  alert("You dont have new notification");
+});
